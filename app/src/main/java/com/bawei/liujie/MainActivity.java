133812e4a16e2809be946fc358c753af175d0144
liujie <862334111@qq.com>
2017-07-10 10:06:21 +0800

    主方法

--- app/src/main/java/com/bawei/liujie/MainActivity.java
+++ app/src/main/java/com/bawei/liujie/MainActivity.java
@@ -1,14 +1,28 @@
 package com.bawei.liujie;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
+/**
+ * 自定义组件：购买数量，带减少增加按钮
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private MySelf mySelf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mySelf = (MySelf) findViewById(R.id.myself);
+        mySelf.setGoods_storage(50);
+        mySelf.setOnAmountChangeListener(new MySelf.OnAmountChangeListener() {
+            @Override
+            public void onAmountChange(View view, int amount) {
+                Toast.makeText(getApplicationContext(), "Amount=>  " + amount, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
